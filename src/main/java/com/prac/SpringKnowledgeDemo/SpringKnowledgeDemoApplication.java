d5f524f493e6769bfd4194052a7b8cf2c48d54aa
nilachalc <cnilachal.19@gmail.com>
2024-07-02 14:09:31 +0530

    initial commit for the project.

---
+++ src/main/java/com/prac/SpringKnowledgeDemo/SpringKnowledgeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prac.SpringKnowledgeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKnowledgeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKnowledgeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prac/SpringKnowledgeDemo/SpringKnowledgeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.SpringKnowledgeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKnowledgeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
